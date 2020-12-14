f99241475da55f31a7cb2ebebed179518d22b651
Kevin Lo <kevlo96@gmail.com>
2020-12-14 09:05:56 -0500

    added projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import AboutMe from './Sections/AboutMe';
 import Intro from './Sections/Intro';
+import Projects from './Sections/Projects';
 
 function App() {
   return (
     <div className="App">
       <Intro />
       <AboutMe />
+      <Projects />
     </div>
   );
 }
---
+++ client/src/Sections/Projects.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Gacha from "../Assets/Images/Gacha Gaming.png"
+import Block from "../Assets/Images/The Block.png"
+import Fashion from "../Assets/Images/Fashion-East.png"
+
+function Projects(props) {
+  return (
+    <div className="project-container">
+      <div className="project-title">Projects</div>
+      <div className="project-div">
+
+        <div className="project">
+          <img src={Gacha} alt="Gacha Gaming" />
+          <div>Gacha Gaming is an gaming app made to emulate gacha games.
+           The app randomly pulls heroes off a data table created on AirTable
+            which the user will than use to battle an enemy until they win or lose.
+            The loss or win is than recorded on the heroes stats back on the data table.
+          </div>
+        </div>
+
+        <div className="project">
+          <img src={Block} alt="The Block" />
+          <div>A web application built in collaboration with a
+          team that provides users  a way to see gentrification,
+          and ways to stop it. Whether it be supporting local
+          events around your block, or contacting government
+          officials to stop it.
+          </div>
+        </div>
+
+        <div className="project">
+          <img src={Fashion} alt="Fashion East" />
+          <div>Want to know what fashion trends are going on
+          with eastern cultures? Fashion-East lets users post
+          what their favorite Korean or Japanese outfits are,
+          and than other users can show their support and comments.
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
