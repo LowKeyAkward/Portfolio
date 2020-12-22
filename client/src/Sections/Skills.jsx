895d76b6b799a944a9f94af7beafd694a7bc3daf
Kevin Lo <kevlo96@gmail.com>
2020-12-22 09:24:43 -0500

    styled skills

--- client/src/Sections/Skills.jsx
+++ client/src/Sections/Skills.jsx
@@ -14,56 +14,64 @@ import "./Skills.css"
 function Skills(props) {
   return (
     <div className="skill-container">
+
+      <div className="skill-title">
+        Skills
+      </div>
       
-      <div className="skill">
-        <img className="skill-image" src={Html} alt="html" />
-        <div className="skill-name">HTML</div>
+      <div className="skill-div">
+        <div className="skill">
+          <img className="skill-image" src={Html} alt="html" />
+          <div className="skill-name">HTML</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={CSS} alt="css" />
+          <div className="skill-name">CSS</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={Js} alt="javascript" />
+          <div className="skill-name">JavaScript</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={Reactic} alt="react" />
+          <div className="skill-name">React</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={Heroku} alt="heroku" />
+          <div className="skill-name">Heroku</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={Express} alt="express" />
+          <div className="skill-name">Express</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={Mongo} alt="mongodb" />
+          <div className="skill-name">MongoDB</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={Post} alt="postgresql" />
+          <div className="skill-name">Postgresql</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={Ruby} alt="ruby" />
+          <div className="skill-name">Ruby</div>
+        </div>
+
+        <div className="skill">
+          <img className="skill-image" src={Rails} alt="rails" />
+          <div className="skill-name">Rails</div>
+        </div>
       </div>
 
-      <div className="skill">
-        <img className="skill-image" src={CSS} alt="css" />
-        <div className="skill-name">CSS</div>
-      </div>
-
-      <div className="skill">
-        <img className="skill-image" src={Js} alt="javascript" />
-        <div className="skill-name">JavaScript</div>
-      </div>
-
-      <div className="skill">
-        <img className="skill-image" src={Reactic} alt="react" />
-        <div className="skill-name">React</div>
-      </div>
-
-      <div className="skill">
-        <img className="skill-image" src={Heroku} alt="heroku" />
-        <div className="skill-name">Heroku</div>
-      </div>
-
-      <div className="skill">
-        <img className="skill-image" src={Express} alt="express" />
-        <div className="skill-name">Express</div>
-      </div>
-
-      <div className="skill">
-        <img className="skill-image" src={Mongo} alt="mongodb" />
-        <div className="skill-name">MongoDB</div>
-      </div>
-
-      <div className="skill">
-        <img className="skill-image" src={Post} alt="postgresql" />
-        <div className="skill-name">Postgresql</div>
-      </div>
-
-      <div className="skill">
-        <img className="skill-image" src={Ruby} alt="ruby" />
-        <div className="skill-name">Ruby</div>
-      </div>
-
-      <div className="skill">
-        <img className="skill-image" src={Rails} alt="rails" />
-        <div className="skill-name">Rails</div>
-      </div>
+      
 
     </div>
   );
