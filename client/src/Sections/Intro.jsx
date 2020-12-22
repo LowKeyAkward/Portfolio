2af2998c13d87a6038cd3c7b6c85351dead97180
Kevin Lo <kevlo96@gmail.com>
2020-12-22 08:55:13 -0500

    added down arrow

--- client/src/Sections/Intro.jsx
+++ client/src/Sections/Intro.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import "./Intro.css"
+import Arrow from "../Assets/Images/down-arrow.png"
 
 function Intro(props) {
   return (
     <div className="intro-container">
+      
       <div className="intro-div">
         <div className="intro">Kevin Lo</div>
         <div className="intro">Aspiring Software Developer</div>
         <div className="intro">Full-Stack Engineer</div>
       </div>
-      <div className="intro-learn">Learn More</div>
+      
+      <div className="down">
+        <div className="intro-learn">Learn More</div>
+        <img className="down-arrown" src={Arrow} alt="down"/>
+      </div>
+      
     </div>
   );
 }
